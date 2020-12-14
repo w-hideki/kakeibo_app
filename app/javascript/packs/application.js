09dae64a2b8e27a6bc710e3435b076e5aa2ba480
w-hideki <73641870+w-hideki@users.noreply.github.com>
2020-12-14 19:26:00 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ require("channels")
 import 'bootstrap';
 import '../stylesheets/application';
 require("chartkick")
-require("Chart.bundle")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
